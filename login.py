c4ee270a60618fba1d609fa06c6dc35dfa00b055
hamza ouabiba <73914495+Hamza-ouabiba@users.noreply.github.com>
2023-04-20 16:59:11 +0000

    Adding name functionality

--- login.py
+++ login.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 import conn
-
+import main as m
 class Login(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
@@ -10,6 +10,7 @@ class Login(QtWidgets.QMainWindow):
         self.ui.connectButton.clicked.connect(self.connect_to_database)
 
     def getLoginPassword(self,login,password,admin_o_n):
+        flag = True
         req = f"select admin from super_utilisateur su join utilisateur u on su.idUser = u.idUser where login='{login}' and mdp='{password}' "
         self.connexion.cursor.execute(req)
         users = self.connexion.cursor.fetchall()
@@ -22,6 +23,7 @@ class Login(QtWidgets.QMainWindow):
                 message.setText("ceci est un compte d'un employé : ")
                 message.setWindowTitle("Error!")
                 message.exec_()
+                flag = False
             elif(admin_o_n == 'Admin' and users[0][0] == 1):
                 message.setText("Connexion reussie")
                 message.setWindowTitle("Nice:)")
@@ -32,20 +34,26 @@ class Login(QtWidgets.QMainWindow):
                 message.setText("ceci est un compte d'un admin")
                 message.setWindowTitle("Error!")
                 message.exec_()
+                flag = False
             elif(admin_o_n == 'Employé' and users[0][0] == 0):
                 message.setText("Connexion reussie")
                 message.setWindowTitle("nice!")
                 message.exec_()
         else:
-            message.setText("Aucun compte enregistré")
+            message.setText("Au cun compte enregistré")
             message.exec_()
+        return flag
     def connect_to_database(self):
         if(self.connexion.connect()):
             login = self.ui.login.text()
             mdp = self.ui.mdp.text()
             choix = self.ui.choix_admin.currentText()
-            self.getLoginPassword(login,mdp,choix)
-
+            check = self.getLoginPassword(login,mdp,choix)
+            # giving access to the main :
+            if(check == True):
+                main_window = m.MainWindow(login)
+                main_window.show()
+                self.hide()
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
