69281281a8810e492c505b365ed94534b5aa3014
hamza ouabiba <73914495+Hamza-ouabiba@users.noreply.github.com>
2023-04-20 15:11:52 +0000

    Update login.py

--- login.py
+++ login.py
@@ -9,21 +9,29 @@ class Login(QtWidgets.QMainWindow):
         self.connexion = conn.Connexion(host="localhost", username="root", password="", database="Location_voiture")
         self.ui.connectButton.clicked.connect(self.connect_to_database)
 
-    def getLoginPassword(self):
-        req = f"select * from utilisateur"
+    def getLoginPassword(self,login,password):
+        req = f"select * from utilisateur where login='{login}' and mdp='{password}'"
         self.connexion.cursor.execute(req)
         users = self.connexion.cursor.fetchall()
         return users
 
     def connect_to_database(self):
         if(self.connexion.connect()):
+            login = self.ui.login.text()
+            mdp = self.ui.mdp.text()
             print("success")
-            users = self.getLoginPassword()
-            print(users)
-    def verifyCoordinates(self):
-        login = self.ui.login.text()
-        mdp = self.ui.mdp.text()
-
+            users = self.getLoginPassword(login,mdp)
+            if (len(users) == 0):
+                message = QtWidgets.QMessageBox()
+                message.setIcon(QtWidgets.QMessageBox.Critical)
+                message.setText("mot de passe ou login incorrecte")
+                message.setWindowTitle("Error!")
+                message.exec_()
+            else:
+                message = QtWidgets.QMessageBox()
+                message.setText("Login et mdp correcte :=)")
+                message.setWindowTitle("nice!")
+                message.exec_()
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = Login()
