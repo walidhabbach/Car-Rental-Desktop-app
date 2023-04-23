adc2d1b9015c982aa6428a6f619057f2a48820b7
hamza ouabiba <73914495+Hamza-ouabiba@users.noreply.github.com>
2023-04-23 23:43:00 +0100

    update

--- GestionClient/Client.py
+++ GestionClient/Client.py
@@ -30,28 +30,34 @@ class Client:
         self.connexion.cursor.execute(req)
         self.connexion.conn.commit()
         print("deleted succesfully")
-    def addClient(self,client_dict):
-        if(self.connexion.connect()):
-            try:
-                #create for the user a login and a password :
-                req1 = f"INSERT INTO utilisateur(`nom`, `prenom`, `login`, `mdp`) VALUES ('{client_dict['nom']}','{client_dict['prenom']}','{client_dict['login']}','{client_dict['mdp']}')"
-                self.connexion.cursor.execute(req1)
-                self.connexion.conn.commit()
 
-                #get the id of the current row :
-                req2 = "SELECT LAST_INSERT_ID() FROM utilisateur"
+    def addClient(self, client_dict):
+        if self.connexion.connect():
+            try:
+                # create for the user a login and a password:
+                req1 = f"INSERT INTO utilisateur(`nom`, `prenom`, `login`, `mdp`) " \
+                       f"VALUES ('{client_dict['nom']}', '{client_dict['prenom']}', '{client_dict['login']}', '{client_dict['mdp']}')"
+                self.connexion.cursor.execute(req1)
+
+                num_rows = self.connexion.cursor.rowcount
+
+                # get the id of the current row:
+                req2 = f"SELECT idUser FROM utilisateur ORDER BY idUser DESC LIMIT {num_rows}"
                 self.connexion.cursor.execute(req2)
                 result = self.connexion.cursor.fetchone()
                 client_id = result[0]
-                print(client_id)
 
-                #creating the client :
-                req3 = f"INSERT INTO client(`idUser` ,`adresse`, `cin`, `photo`, `liste_noire`, `permis`, `passport`, `email`, `observation`," \
-                      f" `societe`, `ville`, `tel`) VALUES ('{client_id}','{client_dict['adresse']}','{client_dict['cin']}',''," \
-                      f"'{client_dict['liste_noire']}','{client_dict['permis']}','{client_dict['passport']}','{client_dict['email']}','{client_dict['observation']}','{client_dict['societe']}','{client_dict['ville']}','{client_dict['tel']}')"
-                print(req3)
+                # creating the client:
+                req3 = f"INSERT INTO client(`idUser`, `adresse`, `cin`, `liste_noire`, `permis`, `passport`, `email`, " \
+                       f"`observation`, `societe`, `ville`, `tel`) " \
+                       f"VALUES ('{client_id}', '{client_dict['adresse']}', '{client_dict['cin']}', " \
+                       f"'{client_dict['liste_noire']}', '{client_dict['permis']}', '{client_dict['passport']}', " \
+                       f"'{client_dict['email']}', '{client_dict['observation']}', '{client_dict['societe']}', " \
+                       f"'{client_dict['ville']}', '{client_dict['tel']}')"
                 self.connexion.cursor.execute(req3)
+
+                # discard any unread result sets:
                 self.connexion.conn.commit()
-                print("Added  succesfully")
+                print("Added successfully")
             except Exception as e:
-                print(f"error : {e}")
+                print(f"Error: {e}")
